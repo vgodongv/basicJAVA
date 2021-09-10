02744914581399a1b4a16505ef3e51d5ee06fa0e
zx449 <zx449@192.168.219.103>
2021-09-11 00:34:20 +0900

    ?/

--- hello/src/hello/hi.java
+++ hello/src/hello/hi.java
@@ -3,7 +3,7 @@ package hello;
 public class hi {
 
 	public static void main(String[] args) {
-		System.out.println("wow µåµð¾î Ã¹ Ä¿¹Ô !");
+		System.out.println("wow µåµð¾î Ã¹ Ä¿¹Ô ! µÎµÕ");
 
 	}
 
