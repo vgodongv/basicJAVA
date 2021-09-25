5ff73bc2da1df89c152e4877228c05e66d7655ec
vgodongv <zx4496@naver.com>
2021-09-25 23:50:44 +0900

    객체지향 메소드 자바

--- e_oop/src/e_oop/Calculator.java
+++ e_oop/src/e_oop/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 	 // 파라미터(계산할때 필요한 값 2개)와 리턴타입(두개의 파라미터를 받아 연산한 결과-알수없으므로 double타입이 적합.) 메서드 5개
 	   
 	   long add(long x,long y){
-	       return x+y;
+	      return x+y;
 	   }
 	   long sub(long x,long y){
 	      return x-y;
